6f2d2d08f2ad07d099ba275c60d0949d66aa17ce
Salmathmullali <135salma7@gmail.com>
2025-03-14 17:15:21 +0000

    fifth commit

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns= [
     path('',views.home,name="home"),
     path('navbar/',views.navbar,name="navbar"),
+    path('normal_user_page/',views.normal_user_page,name="normal_user_page"),
+    path('seller_page/',views.seller_page,name="seller_page"),
+    path('charity_page/',views.charity_page,name="charity_page"),
     path('register/',views.registration,name="registration"),
-    path('user_reg/',views.user_reg,name="user_reg")
+    path('user_reg/',views.user_reg,name="user_reg"),
+    path('charity_user_reg/',views.charity_user_reg,name="charity_user_reg"),
+    path('seller_reg/',views.seller_reg,name="seller_reg")
+
 ]
--- app/views.py
+++ app/views.py
@@ -9,6 +9,12 @@ def home(request):
 
 def navbar(request):
     return render(request, "navbar.html")
+def charity_page(request):
+    return render(request, "charity_page.html")
+def seller_page(request):
+    return render(request, "seller_page.html")
+def normal_user_page(request):
+    return render(request, "normal_user_page.html")
 
 def registration(request):
     return render(request, "register.html")
@@ -23,8 +29,42 @@ def user_reg(request):
             user.save()
             login(request, user)
             messages.success(request, 'Registration successful!')
-            return redirect('navbar')
+            return redirect('normal_user_page')
         else:
             messages.error(request, 'Error occurred during registration. Please check your inputs.')
 
     return render(request, 'user_reg.html', {'form': form})
+
+def charity_user_reg(request):
+    form = MyUserCreationForm()
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.email  # Use email as username
+            user.save()
+            login(request, user)
+            messages.success(request, 'Registration successful!')
+            return redirect('charity_page')
+        else:
+            messages.error(request, 'Error occurred during registration. Please check your inputs.')
+
+    return render(request, 'user_reg.html', {'form': form})
+
+def seller_reg(request):
+    form = MyUserCreationForm()
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.email  # Use email as username
+            user.save()
+            login(request, user)
+            messages.success(request, 'Registration successful!')
+            return redirect('seller_page')
+        else:
+            messages.error(request, 'Error occurred during registration. Please check your inputs.')
+
+    return render(request, 'user_reg.html', {'form': form})
+
+
